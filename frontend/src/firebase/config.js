f80aad9ae5897086752357dff9a0f41af4c15ff7
Danilo P Catalan <danilo.e.catalan@gmail.com>
2021-09-01 22:21:48 -0400

    feature(products): se agrega Backend con un endpoint de productos para ser consumido desde el Frontend para completar la funcionalidad de listar productos y autenticarse en la aplicación

---
+++ frontend/src/firebase/config.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: 'AIzaSyCiHetYeO_9d4pP_CYq-wfOTaTKtQ3JAwM',
+  authDomain: 'authenticator-boolean.firebaseapp.com',
+  projectId: 'authenticator-boolean',
+  storageBucket: 'authenticator-boolean.appspot.com',
+  messagingSenderId: '608440268250',
+  appId: '1:608440268250:web:861fd78781f1d33b7ebce3'
+}
---
+++ frontend/tests/e2e/specs/login.js
@@ -0,0 +1,15 @@
+// https://docs.cypress.io/api/introduction/api.html
+
+describe('login test suite', () => {
+  it('does not work with wrong credentials', () => {
+    cy.login('info', 'visitor')
+
+    cy.location('pathname').should('equal', '/')
+  })
+
+  it('does work with valid credentials', () => {
+    cy.login('test-e2e@boolean.cl', 'booleanacademia')
+    cy.location('pathname').should('equal', '/productos')
+  })
+
+})
